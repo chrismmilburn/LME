e30b24cf9986008d140efd97aa775b26f31f6dae
chrismmilburn <chrismmilburn@netscape.net>
2021-10-27 08:51:44 +0100

    Added direction handling and user input handling.

--- src/main/java/robot/RobotControl.java
+++ src/main/java/robot/RobotControl.java
@@ -1,13 +1,14 @@
 package robot;
 
 import utils.Compass;
+import utils.RobotCommands;
 
 import java.util.Scanner;
 
 public class RobotControl {
-    private int xPosition;
-    private int yPosition;
-    private Compass bearing;
+    private int robotXPosition;
+    private int robotYPosition;
+    private Compass robotBearing;
     private int maxXSize;
     private int maxYSize;
     private Boolean robotLost;
@@ -23,11 +24,51 @@ public class RobotControl {
     public String commandRobots(Scanner input) {
         String finalBearings="";
 
+        maxXSize = input.nextInt();
+        maxYSize = input.nextInt();
 
+        while(input.hasNext()) {
+
+            robotXPosition = input.nextInt();
+            robotYPosition = input.nextInt();
+            robotLost = false;
+            String startingOrientation = input.next();
+            robotBearing = Compass.valueOf(startingOrientation);
+
+            String commands = input.next();
+            finalBearings += processCommands( commands );
+
+            System.out.println(finalBearings);
+        }
 
         return finalBearings;
     }
 
+    private String processCommands(String commandString){
 
+        char[] commands = commandString.toCharArray();
+        int currentCommand = 0;
+        char command;
+
+        while( (currentCommand < commands.length )&&(robotLost != true)) {
+            command = commands[currentCommand++];
+            switch (RobotCommands.valueOf(String.valueOf(command))) {
+                case L:
+                    robotBearing = Compass.turnLeft(robotBearing);
+                    break;
+                case R:
+                    robotBearing = Compass.turnRight(robotBearing);
+                    break;
+                case F:
+ //                   moveForward();
+                    break;
+                default:
+                    System.out.println("Unknown command "+command);
+            }
+        }
+        String resultLocation = robotXPosition+" "+robotYPosition+" "+robotBearing+(robotLost?" LOST":"");
+
+        return resultLocation;
+    }
 
 }
